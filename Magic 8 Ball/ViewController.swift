428217620f8952be71a3eb6323052dec758f2a9e
darshilagrawal <darshilagrawal2000@gmail.com>
2021-09-29 23:10:20 +0530

    Indentation fixes

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -6,16 +6,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageBall: UIImageView!
     let ballArray = [#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball4")]
-
-
-
+    
     @IBAction func pressedButton(_ sender: UIButton) {
         UIView.animate(withDuration: 0.1,
                        delay: 0.1,
                        options: UIView.AnimationOptions.curveEaseIn,
                        animations: { () -> Void in
-                        self.imageBall.image=self.ballArray.randomElement()
-            }, completion: { (finished) -> Void in
+            self.imageBall.image=self.ballArray.randomElement()
+        }, completion: { (finished) -> Void in
         })
         
     }
